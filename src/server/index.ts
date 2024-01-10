5b2daa9b82f2fe25c3370ae366d8857fc2f110ba
Yordi Cartagena <yordicartagena24@gmail.com>
2024-01-10 15:43:20 -0400

    Add basic chat functionality

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,27 @@
 import express, { Express, Request, Response} from 'express'
 import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const port = process.env.PORT || 3000
 
 const app: Express = express()
+const server = createServer(app)
+const io = new Server(server, {
+   connectionStateRecovery: {} 
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+
+    socket.on('chatMessage', (msg) => {
+        console.log('message: ' + msg)
+        io.emit('chatMessage', msg)
+    })
+})
 
 app.use(logger('dev'))
 
@@ -11,6 +29,6 @@ app.get('/', (req: Request, res: Response) => {
     res.sendFile(process.cwd() + '/src/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running at: 🚀 http://localhost:${port}`)
 })
